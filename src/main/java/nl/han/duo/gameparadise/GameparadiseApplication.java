bb53063d219e53b4d97022ad3e71b823b71b209c
Joey Stoffels <joey.stoffels@exe.energy>
2017-12-12 15:56:43 +0100

    Initial commit

---
+++ src/main/java/nl/han/duo/gameparadise/GameparadiseApplication.java
@@ -0,0 +1,12 @@
+package nl.han.duo.gameparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameparadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameparadiseApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/han/duo/gameparadise/GameparadiseApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.han.duo.gameparadise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameparadiseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
