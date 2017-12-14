18ee0c32e5c48b3362a8664493dce9c122d19154
Nick Hartjes <info@nickhartjes.nl>
2017-12-14 21:37:02 +0100

    Default view database files

---
+++ src/main/java/nl/han/duo/gameparadise/repository/TestViewClassRepository.java
@@ -0,0 +1,9 @@
+package nl.han.duo.gameparadise.repository;
+
+
+import nl.han.duo.gameparadise.dao.TestViewClass;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TestViewClassRepository extends CrudRepository<TestViewClass, Long> {
+
+}
